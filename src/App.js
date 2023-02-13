49d0d3e275991230c13a7215aa678f6accc796b0
simple-sifu <kattyntommy@gmail.com>
2023-02-13 12:44:38 -0500

    initial version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from 'react-router-dom';
+import '../App.css';
+import {FaCalendarAlt, FaDoorOpen, FaUsers} from "react-icons/fa";
+
+import BookablesPage from './Bookables/BookablesPage';
+import BookingsPage from './Bookings/BookingsPage';
+import UsersPage from './Users/UsersPage';
+import UserPicker from './Users/UserPicker';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/bookings" className="btn btn-header">
+                <FaCalendarAlt />
+                <span>Bookings</span>
+              </Link>
+            </li>
+            <li>
+              <Link to="/bookables" className="btn btn-header">
+                <FaDoorOpen />
+                <span>Bookables</span>
+              </Link>
+            </li>
+            <li>
+              <Link to="/users" className="btn btn-header">
+                <FaUsers />
+                <span>Users</span>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        <UserPicker />
       </header>
+      <Routes>
+        <Route path="/bookings" element={<BookingsPage/>} />
+        <Route path="/bookables" element={<BookablesPage />} />
+        <Route path="/users" element={<UsersPage />} />
+      </Routes>
     </div>
-  );
+  );``
 }
 
 export default App;
